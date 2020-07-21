20b9b4c9175045641047d665eefcf78d78696f4e
GraziellaDuarte18 <grazielladuarte86@gmail.com>
2020-07-20 22:43:29 -0300

    Correções no calculo de imc

--- imc.js
+++ imc.js
@@ -1,14 +1,17 @@
-var nome = prompt("Digite seu nome");
-var altura = prompt("Digite sua altura");
-var peso = prompt("Digite seu peso");
-imc = peso / (altura * altura);
+function clicar() {
+    var nome
+    var altura = document.querySelector(".altura").value;
+    var peso = document.querySelector(".peso").value;
+    var imc = peso / (altura * altura);
+    document.querySelector(".imc").innerHTML = imc
 
-if (imc < 18.5) {
-    document.write(nome + "Você está abaixo do peso")
-} else if (imc > 18.5 && imc < 25) {
-    document.write(nome + "Você está no peso ideal")
-} else if (imc > 25 && imc < 30) {
-    document.write(nome + "Você está acima do peso")
-} else if (imc > 30) {
-    document.write(nome + "Você está com obesidade morbida");
+    if (imc < 18.5) {
+        document.write(nome + "Você está abaixo do peso")
+    } else if (imc > 18.5 && imc < 25) {
+        document.write(nome + "Você está no peso ideal")
+    } else if (imc > 25 && imc < 30) {
+        document.write(nome + "Você está acima do peso")
+    } else if (imc > 30) {
+        document.write(nome + "Você está com obesidade morbida");
+    }
 }
